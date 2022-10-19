1edabf2d0e3b95f93cbc33d618c0c9c87e35cd99
FecheIannitto <fmiannitto@gmail.com>
2022-10-19 00:36:55 -0300

    Formulario para completar datos de Tarjeta de Credito

---
+++ comando.js
@@ -0,0 +1,8 @@
+const nombre = document.getElementById("nombre")
+const nombreInput = document.getElementById("nombreInput")
+
+function duplicar () {
+
+    nombre.value = nombreInput.value
+    console.log(nombre.value)
+}
