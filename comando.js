0e59cebc09422bc85be8d71c4059f2711e5dfa3b
FecheIannitto <fmiannitto@gmail.com>
2022-10-26 02:41:30 -0300

    Parcial

--- comando.js
+++ comando.js
@@ -1,11 +1,52 @@
-const cvcForm = document.getElementById("cvcForm")
-const cvcBack = document.getElementById("cvcBack")
+const cvcInput = document.getElementById("cvcInput")//---> input del codigo de seguridad tarjeta (formulario)
+const cvcBack = document.getElementById("cvcBack") //---> span del codigo de seguridad tarjeta
+const fecha = document.querySelector(".inputFront-vencimiento")
+const mes = document.getElementById("mes")
+const año = document.getElementById("año")
 
 
-const formulario = document.querySelector('formulario');
+const ladoDer = document.querySelector('.ladoDer');
 
-function igualar() {
-    document.getElementById("cvcForm").value =  document.getElementById("cvcBack").value
-}
 
-console.log(cvcBack)
+
+cvcInput.addEventListener("input", (event) => {
+
+    cvcBack.textContent = event.target.value
+
+   // console.log(event.target.value)
+
+
+})
+
+mes.addEventListener("input", (event) => {
+
+
+    fecha.children[0].textContent = event.target.value
+
+    //console.log(fecha.children[0])
+
+
+})
+
+
+
+año.addEventListener("input", (event) => {
+
+    fecha.children[1].textContent = event.target.value
+
+   // console.log(event.target.value)
+
+
+})
+
+
+ladoDer.addEventListener("submit", (e) => {
+
+    e.preventDefault()
+    const formulario = document.querySelector(".formulario")
+    formulario.innerHTML = '<button class="confirmar">Continuar</button>'
+
+
+})
+
+//console.log(ladoDer)
