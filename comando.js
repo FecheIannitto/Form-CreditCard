38b71c44b2a869215df2a1e3b6ec5dd8bea58886
Federico <fmiannitto@gmail.com>
2022-10-31 01:20:35 -0300

    Parcial

--- comando.js
+++ comando.js
@@ -18,7 +18,7 @@ nombre.addEventListener("input", (event) => {
 
 
 numero.addEventListener("input", (event) => {
-    numeroFront.textContent = event.target.value 
+    numeroFront.textContent = event.target.value
 })
 
 
@@ -44,11 +44,64 @@ ladoDer.addEventListener("submit", (e) => {
     if (!nombre.value || !numero.value || !cvcInput.value || !mes.value || !año.value) {
         alert('Faltan datos')
     } else {
-        const formulario = document.querySelector(".formulario") // busca el div del formulario
-        formulario.innerHTML = '<button class="confirmar">Continuar</button>' //crea otro boton post-submit con la misma clase que el primero, borrando el div formulario
+
+        //OPCION 3
         
-        
-       
+        const gracias = document.getElementById("gracias")
+        const nene = document.getElementById("nene")
+
+        nene.style = "display: none;"        
+        gracias.style = "text-align: center;"
+
+
+
+
+
+
+        //OPCION 2
+
+        // const formulario = document.getElementById("ladoDer") // busca el div padre del formulario
+        // const nene = document.getElementById("nene")
+        // const div = document.createElement("div")
+        // div.setAttribute("class", "formulario")
+        // div.style = "text-align: center;"
+        // // creamos etiqueta IMG
+        // const img = document.createElement("img")
+        // //seteamos atributos ALT y SRC
+        // img.alt = "check"
+        // img.src = "icon-complete.svg"
+        // //agregamos al div padre
+        // div.appendChild(img)
+        // //creamos el H1
+        // const titulo = document.createElement("h1")
+        // titulo.textContent = "Gracias!"
+        // div.appendChild(titulo)
+        // //creamos el <p>
+        // const mensaje = document.createElement("p")
+        // mensaje.textContent = "Tenemos agregados los detalles de tu tarjeta"
+        // div.appendChild(mensaje)
+        // mensaje.style = "font-size: 15px;"
+        // //creamos el botton
+        // const button = document.createElement("button")
+        // button.textContent = "Continuar"
+        // button.setAttribute("class", "confirmar")
+        // div.appendChild(button)
+        // //remueve del div padre, el hijo (nene)
+        // formulario.removeChild(nene)
+        // //agregamos al div padre, el div creado
+        // formulario.appendChild(div)
+
+        //OPCION 1
+
+        //     <div class="formulario" style="text-align: center;">
+        //     <img src="icon-complete.svg" alt="check">      
+        //     <h1>Gracias!</h1>
+        //     <p>Pudimos agregar los detalles de tu tarjeta</p>
+        //     <button class="confirmar">Continuar</button>
+        // </div>` //crea otro boton post-submit con la misma clase que el primero, borrando el div formulario
+
+
+
     }
 
 })
